8aba8e987742fdeb6591a49961709c28d1221ee8
Sibar Al-ani <sibar.alani@gmail.com>
2026-09-01 10:02:57 +0200

    comments in routes, embedded

--- week7-joins-&-auth-&-client/lecture-18/02-todosapi-mongoDB-embedded/src/routes/todos.ts
+++ week7-joins-&-auth-&-client/lecture-18/02-todosapi-mongoDB-embedded/src/routes/todos.ts
@@ -12,15 +12,17 @@ import {
 const router = express.Router()
 
 // Todo routes
-router.get('/', fetchAllTodos)
-router.get('/:id', fetchTodo)
-router.post('/', createTodo)
-router.patch('/:id', updateTodo)
-router.delete('/:id', deleteTodo)
+router.get('/', fetchAllTodos) // localhost:3000/todos
+router.get('/:id', fetchTodo)  // localhost:3000/todos/:id
+router.post('/', createTodo)   // localhost:3000/todos
+router.patch('/:id', updateTodo) // localhost:3000/todos/:id
+router.delete('/:id', deleteTodo) // localhost:3000/todos/:id
+
+
 
 // Subtask routes (nested under todo)
-router.post('/:id/subtasks', createSubtask)
-router.patch('/:id/subtasks/:subtaskId', updateSubtask)
-router.delete('/:id/subtasks/:subtaskId', deleteSubtask)
+router.post('/:id/subtasks', createSubtask) // localhost:3000/todo/:id/subtasks
+router.patch('/:id/subtasks/:subtaskId', updateSubtask)   // localhost:3000/todo/:id/subtasks/:subtaskid
+router.delete('/:id/subtasks/:subtaskId', deleteSubtask) // localhost:3000/todo/:id/subtasks/:subtaskid
 
 export default router;

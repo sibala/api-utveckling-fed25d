e3d5d56301a51b772c2e0e1edec906c598749d87
Sibar Al-ani <sibar.alani@gmail.com>
2026-09-08 13:02:50 +0200

    ajustments

--- week8-authapi-with-client-and-AI/lecture-20/starter-app-template/src/controllers/greetingController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express"
-
-export const greetingSpecific = async (req: Request, res: Response) => {
-    const name = req.params.name
-
-    res.json({message: `Hello ${name}, welcome to your admin panel!`})
-}
--- week8-authapi-with-client-and-AI/lecture-20/starter-app-template/src/routes/greetings.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {greetingSpecific} from '../controllers/greetingController'
-import { verifyToken } from '../middleware/verifyToken'
-const router = express.Router()
-
-router.get('/:name', verifyToken, greetingSpecific)
-
-export default router
